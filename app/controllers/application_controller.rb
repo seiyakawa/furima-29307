c37db9da8a2d1a2682aa0acc8558a451003d9476
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-11 20:41:10 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
-
+  before_action :authenticate_user!
+# ログインしていない場合は、ログイン画面に遷移
+  before_action :configure_permitted_parameters, if: :devise_controller?
+# すべてのアクションの前に実行
   private
 
   def basic_auth
@@ -9,5 +11,12 @@ class ApplicationController < ActionController::Base
       # username == "furima" && password == "1111"
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
-  end    
+  end   
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  # ストロングパラメーター [nickname]キーの内容の保存をpermitメソッドで許可
 end
