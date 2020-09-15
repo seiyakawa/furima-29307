6239bde92ad21c714d76ae3dbf6042c14ab6127a
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-15 10:00:40 +0900

    AcriveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  # ActiveHashを使用してアソシエーションを設定
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
+  #空の投稿を保存できないように設定
+  validates :name, :text, :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, price, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないように設定
+  validates :category_id, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, numericality: { other_than: 0 } 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_103150) do
+ActiveRecord::Schema.define(version: 2020_09_14_232414) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.integer "category_id", null: false
+    t.integer "sales_status_id", null: false
+    t.integer "shipping_fees_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "scheduled_delivery_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
