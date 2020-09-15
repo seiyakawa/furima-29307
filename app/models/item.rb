c7e66d8561d93cac33fc4a2d0533d868a5907955
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-15 17:50:26 +0900

    バリデーションの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
 
-
-  
-  #空の投稿を保存できないように設定
-  validates :name, :text, :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, :price, presence: true
+ #空の投稿を保存できないように設定
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    # 価格の入力は半角数字のみ 範囲は300から9,999,999
+    validates :price, format: { with: /\A[0-9]+\z/, message: "Half width"} , inclusion: {in: 300..9999999, message: "Out of setting range" } 
+    validates :category
+    validates :sales_status
+    validates :shipping_fee_status
+    validates :prefecture
+    validates :scheduled_delivery
+  end
 
   #ジャンルの選択が「--」の時は保存できないように設定
-  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 0 } 
+  with_options numericality: { other_than: 0, message: "Select"} do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
 end
