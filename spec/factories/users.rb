701356724a7dd8a518b7198f3751d0972c7f58f4
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-17 15:52:40 +0900

    Rubocop修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
+    nickname { Faker::Name.name}
     email {Faker::Internet.free_email}
     password {'abc123'}
     password_confirmation {password}
