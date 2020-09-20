bc858f8f5055c43e8636f8483bace923e739454a
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-20 21:53:11 +0900

    ログインページまたはトップページへの条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   # 購入画面に行きたいユーザーがログインしていなければ、ログイン画面へ遷移させる
-  before_action :authenticate_user!
+  before_action :move_to_top_page
 
   def index
+    authenticate_user!
     @item = Item.find(params[:item_id])
     @order = OrderAddress.new
   end
@@ -36,5 +37,14 @@ class OrdersController < ApplicationController
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類(日本円)
     )
+  end
+
+  def move_to_top_page
+    @item = Item.find(params[:item_id])
+    if user_signed_in? != true
+      redirect_to new_user_session_path
+    elsif @item.user_id == current_user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
 end
