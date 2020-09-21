c19bfe81dbcd196f91cb079aac642db4fd4620f0
seiyakawa <seiya.kawashima117@gmail.com>
2020-09-21 15:25:38 +0900

    Revert "ログインへの遷移"
    This reverts commit 37658fcade72ca95ab07fd035df8617aebd69c2e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,9 +50,7 @@ class ItemsController < ApplicationController
 
   # 未ログインのユーザーをログインページへ遷移
   def move_to_login
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def set_item
